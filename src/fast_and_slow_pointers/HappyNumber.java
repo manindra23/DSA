ce2571fc80f261e2fed983205d84950fd4058304
Manindra Singh <manindra23@yahoo.co.in>
2025-07-27 07:01:10 +0530

    Happy Number problem- adding comments

--- src/fast_and_slow_pointers/HappyNumber.java
+++ src/fast_and_slow_pointers/HappyNumber.java
@@ -1,5 +1,21 @@
 package fast_and_slow_pointers;
 
+/*
+Write an algorithm to determine if a number n is a happy number.
+
+We use the following process to check if a given number is a happy number:
+
+Starting with the given number n, replace the number with the sum of the squares of its digits.
+Repeat the process until:
+The number equals 1, which will depict that the given number n is a happy number.
+The number enters a cycle, which will depict that the given number n is not a happy number.
+
+Return TRUE if n is a happy number, and FALSE if not.
+
+Constraints:
+    + 1<=n<= 2^31 - 1
+
+ */
 public class HappyNumber {
     public static boolean isHappyNumber(int n) {
         int sum = 0;
